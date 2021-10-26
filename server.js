d851cf5c0949a3e997ff969baf5781f4ddcf6c3f
loicbdev <loicbodolec@msn.com>
2021-10-26 09:43:30 +0200

    add comments

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var { graphqlHTTP} = require('express-graphql');
+var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 // GraphQL schema
@@ -19,6 +19,7 @@ var app = express();
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     rootValue: root,
+    // graphiql: Must be set to true to enable the GraphiQL tool when accessing the endpoint in the browser. GraphiQL is a graphical interactive in-browser GraphQL IDE. By using this tool you can directly write your queries in the browser and try out the endpoint.
     graphiql: true
 }));
 app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
